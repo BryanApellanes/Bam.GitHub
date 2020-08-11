bee54c9e6d2d59d03134f1391e18e51c0204ae92
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-08-11 09:29:19 -0700

    default customer response

---
+++ CustomerResponses/CustomerResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Bam.GitHub.CustomerResponses
+{
+    public class CustomerResponse
+    {
+        public CustomerResponse()
+        {
+            Text = "Thank you for reaching out, and I'm sorry you're seeing that error. We will review further internally to gain understanding as to the root of the issue. We'll provide updates here when there is more information to share.";
+        }
+        
+        public string Text { get; set; }
+        
+        /// <summary>
+        /// The id of the github issue or pull request
+        /// </summary>
+        public string GitHubItemId { get; set; }
+    }
+}
