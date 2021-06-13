4d428e7bb860a82bc53c99a01a573e7a9e973e23
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-06-12 19:19:41 -0700

    wip

---
+++ Data/OwnedRepoListData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Github.Data
+{
+    public class OwnedRepoListData
+    {
+        public string Owner{ get; set; }
+        public string[] Repositories { get; set; }
+    }
+}
